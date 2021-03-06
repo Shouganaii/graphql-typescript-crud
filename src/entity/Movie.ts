25af912203c479ee51cc3775c05efc757661b11e
Shouganaii <frias.caio@yahoo.com.br>
2021-03-06 19:50:19 -0300

    first commit

---
+++ src/entity/Movie.ts
@@ -0,0 +1,20 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@ObjectType()
+@Entity()
+export class Movie extends BaseEntity {
+
+    @Field(() => Int)
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Field()
+    @Column('text')
+    title: string
+
+    @Field(() => Int)
+    @Column('int', { default: 60 })
+    minutes: number
+
+}
